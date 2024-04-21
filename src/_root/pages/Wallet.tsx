c51095b482532a2775ae26771f07373e6c79ff8a
omariosman <omarosman23@aucegypt.edu>
2024-04-21 09:04:50 +0200

    :fire: feat: wallet page

--- src/_root/pages/Wallet.tsx
+++ src/_root/pages/Wallet.tsx
@@ -1,18 +1,40 @@
 import { useToast } from "@/components/ui/use-toast";
 import { Loader, UserCard } from "@/components/shared";
+import { useNavigate } from "react-router-dom";
 
 import { NFTCard } from "@/components/shared";
-
-const Wallet = () => {
+type NFTCardProps = {
+    name: string, 
+    network: string
+  };
+const Wallet = ({ nfts1 }: NFTCardProps[]) => {
   const { toast } = useToast();
-    let nft = {
-        name: "nft #1",
-        network: "base"
-    }
+    let nfts: NFTCardProps[] = 
+    [
+        {
+            name: "nft #1",
+            network: "base",
+            image: "https://thumbor.forbes.com/thumbor/fit-in/x/https://www.forbes.com/advisor/in/wp-content/uploads/2022/03/monkey-g412399084_1280.jpg"
+        },
+        {
+            name: "nft #2",
+            network: "base",
+            image: "https://i.guim.co.uk/img/media/b8a75934f827bdaf02a3814d1669c8da19886881/0_727_3500_2100/master/3500.jpg?width=465&dpr=1&s=none"
+        }
+    ]
+
+    
   return (
     <div className="common-container">
         Hello Wallet
-      <NFTCard nft={nft}/>
+        <ul className="flex flex-col flex-1 gap-9 w-full ">
+              {nfts.map((nft: any, index: any) => (
+                <li key={index} className="flex justify-center w-full">
+                  <NFTCard nft={nft}/>
+                </li>
+              ))}
+            </ul>
+      
     </div>
   );
 };
--- src/components/shared/NFTCard.tsx
+++ src/components/shared/NFTCard.tsx
@@ -1,28 +1,39 @@
 import { Models } from "appwrite";
 import { Link } from "react-router-dom";
-
+import { Button } from "@/components/ui";
 import { PostStats } from "@/components/shared";
 import { multiFormatDateString } from "@/lib/utils";
+import { useNavigate } from "react-router-dom";
 
 type NFTCardProps = {
-  name: string, 
-  network: string
+  name: string,
+  network: string,
+  image: string
 };
 
-// name
-// network
-// image
 const NFTCard = ({ nft }: NFTCardProps) => {
+  const navigate = useNavigate();
 
   return (
     <div className="post-card">
       <div className="flex-between">
         <div className="flex items-center gap-3">
-        nft
+          <p>{nft.name}</p>
+          <p>{nft.network}</p>
+          {/* <img
+            src={nft.image}
+            alt="nft image"
+            className="post_details-img"
+          /> */}
         </div>
 
+        <Button
+          type="button"
+          className="shad-button_dark_4"
+          onClick={() => navigate("/create-post")}>
+            Review
+        </Button>
       </div>
-
     </div>
   );
 };
